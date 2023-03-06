9d62c2300758ca47a720e62d4cab8aae14af8144
Denjiken <Denjiken>
2023-03-07 00:18:35 +0800

    on going

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+<div class="logo">
+    <a href="javascript:;" class="simple-text">
+        <img src="https://dashboard.kingnewswire.com/uploads/press_release/167698450863f4c0bcd4537167698450863f4c0bcd4538.png" alt="Your Logo"
+        width="130px">
+    </a>
+</div>
+
+<ul class="nav">
+    <li class="nav-item active">
+        <a class="nav-link" href="transaction.php">
+            <i class="nc-icon nc-icon nc-paper-2"></i>
+            <p>Transaction</p>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link" href="office.php">
+            <i class="nc-icon nc-bank"></i>
+            <p>Office</p>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link" href="employee.php">
+            <i class="nc-icon nc-alien-33"></i>
+            <p>Employee</p>
+        </a>
+    </li>
+
+    <li class="nav-item active active-pro">
+        <a class="nav-link active" href="javascript:;">
+            <i class="nc-icon nc-alien-33"></i>
+                <p>Upgrade plan</p>
+        </a>
+    </li>   
+</ul>                     
